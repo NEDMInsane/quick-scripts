a1f4e56e356be246d544c867c80388e5f27511da
Nicholas Truxal <truxaln@gmail.com>
2022-09-17 19:23:56 -0500

    Update uniquegenerationtillhelloworld.py

--- Python/uniquegenerationtillhelloworld.py
+++ Python/uniquegenerationtillhelloworld.py
@@ -38,22 +38,27 @@ def generate_string():
     num_iterations = 0
     proposed_string = ""
     while num_iterations < 11:
+        # Generate a random string of 12 characters total
         random_num = rand.randint(1, 27)
         add_letter = letters[random_num]
         proposed_string = proposed_string + add_letter
         num_iterations = num_iterations + 1
 
     if proposed_string in hello_world_list:
+        # If the string is already in the list, generate a new string
         proposed_string = ""
         generate_string()
     elif proposed_string not in hello_world_list:
+        # If the string is not in the list, add it to the list, and return it
         hello_world_list.append(proposed_string)
         print(len(hello_world_list))
         return proposed_string
 
 
 while hello_world_string != "hello world":
+    # Runs this script until hello_word_string = hello world
     hello_world_string = generate_string()
     print(hello_world_string)
 
+# This isnt implemented yet rip
 print("This took " + str(i) + " iterations")
